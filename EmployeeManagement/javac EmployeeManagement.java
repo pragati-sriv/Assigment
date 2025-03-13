08b323c847bd2979878f9177007825ac5f4c69ae
Pragati Srivastava <pragati.srivastava@yesbossai.com>
2025-03-13 14:14:06 +0530

    management

---
+++ EmployeeManagement/javac EmployeeManagement.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Salary: $" + salary);
+    }
+}
+
+
+public class EmployeeManagement {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new Employee(101, "Shikha", 50000));
+        employees.add(new Employee(102, "Ankit", 60000));
+        employees.add(new Employee(103, "Simran", 55000));
+
+        System.out.println("Employee Details:");
+        for (Employee emp : employees) {
+            emp.displayDetails();
+        }
+    }
+}
